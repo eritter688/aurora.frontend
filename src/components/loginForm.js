45f24dc9dfc384301f867cfc2b481c52e2ce9dfb
Eric <eritter688@gmail.com>
2020-05-07 16:42:33 -0400

    can actually kinda 'login' now...

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -21,31 +21,24 @@ export default class LoginForm extends Component {
 
     submitHandler = (event) => {
         event.preventDefault(); // why?
-        login(this.state.email, this.state.password)
+        login(this.state.email, this.state.password);
     }
 
     render() {
         return (
             <Form onSubmit={this.submitHandler}>
-                <Form.Group controlId="formBasicEmail">
+                <Form.Group controlId="loginFormEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email"
                                   name="email"
                                   onChange={this.inputHandler}/>
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
                 </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
+                <Form.Group controlId="loginFormPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password"
                                   name="password"
                                   onChange={this.inputHandler}/>
                 </Form.Group>
-                {/*<Form.Group controlId="formBasicCheckbox">*/}
-                {/*    <Form.Check type="checkbox" label="Check me out"/>*/}
-                {/*</Form.Group>*/}
                 <Button variant="primary" type="submit">
                     LOGIN
                 </Button>
