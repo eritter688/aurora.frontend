1a51260a89150e225070149c888846e22d8c55af
Eric <eritter688@gmail.com>
2020-05-23 15:34:45 -0400

    footer enhancements

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,55 +1,53 @@
 import React from 'react';
-import NavBar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav"
+import NavBar from "react-bootstrap/Navbar";
 import NavItem from "react-bootstrap/NavItem";
 import {Link} from "react-router-dom";
 
-
+// TODO
+// Debating on making this a tack on footer style where it's appended to the body
+// content, as opposed to sticky on the bottom of the SPA.
 export default function Footer() {
 
-    // TODO
-    // On phone, this styles into vertical, taking up a lot of screen
-    // real estate.
     const FooterStyle = {
         fontVariant: "all-small-caps",
-        fontSize: ".80em",
+        fontSize: ".75em",
+
         borderTopStyle: "solid",
         borderWidth: "1px",
         borderColor: "lightgray",
+
+        height: "5%",
     };
 
     return (
         <NavBar style={FooterStyle}
                 fixed={"bottom"}
                 bg={"light"}
-                variant={"light"}
-                expand={"sm"}>
-            <NavBar.Toggle aria-controls="responsive-navbar-nav-left"/>
-            <NavBar.Collapse id="responsive-navbar-nav-left">
-                <Nav className={"mr-auto"}>
-                    <NavItem>
-                        <Nav.Link as={Link} to={"/about/"}>About</Nav.Link>
-                    </NavItem>
-                    <NavItem>
-                        <Nav.Link as={Link} to={"/terms/"}>Terms</Nav.Link>
-                    </NavItem>
-                    <NavItem>
-                        <Nav.Link as={Link} to={"/privacy/"}>Privacy</Nav.Link>
-                    </NavItem>
-                    <NavItem>
-                        <Nav.Link as={Link} to={"/bug/"}>Contact/Report Bug</Nav.Link>
-                    </NavItem>
-                    <NavItem>
-                        <Nav.Link as={Link} to={"/help/"}>Help</Nav.Link>
-                    </NavItem>
-                </Nav>
-            </NavBar.Collapse>
+                variant={"light"}>
+            <Nav className={"mr-auto"}>
+                <NavItem>
+                    <Nav.Link as={Link} to={"/about/"}>About</Nav.Link>
+                </NavItem>
+                <NavItem>
+                    <Nav.Link as={Link} to={"/terms/"}>Terms</Nav.Link>
+                </NavItem>
+                <NavItem>
+                    <Nav.Link as={Link} to={"/privacy/"}>Privacy</Nav.Link>
+                </NavItem>
+                <NavItem>
+                    <Nav.Link as={Link} to={"/bug/"}>Bug</Nav.Link>
+                </NavItem>
+                <NavItem>
+                    <Nav.Link as={Link} to={"/help/"}>Help</Nav.Link>
+                </NavItem>
+            </Nav>
             <Nav className={"ml-auto"}>
                 <NavItem>
                     <Nav.Link as={Link} to={"/api/"}>{'<'}/{'>'}</Nav.Link>
                 </NavItem>
                 <NavItem>
-                    <Nav.Link eventKey={"disabled"} disabled>C 2020 Aurora LLC.</Nav.Link>
+                    <Nav.Link as={Link} to={"/copyright/"}>&copy;</Nav.Link>
                 </NavItem>
             </Nav>
         </NavBar>
