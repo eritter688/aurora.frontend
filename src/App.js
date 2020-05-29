cc1497a680ca3f0f4064e9599c51285a8958ef65
Eric <eritter688@gmail.com>
2020-05-29 16:19:24 -0400

    in progress navbar with css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import authService from './services/tokenService';
 import {useDispatch} from "react-redux";
 import {api} from "./axios/axios";
